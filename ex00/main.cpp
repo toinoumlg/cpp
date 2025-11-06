56833a7c0cb989343328200276ff3c6cd3177c8d
toinoumlg <antoine.malanguerray@gmail.com>
2025-11-06 12:31:17 +0100

    initial commit

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,33 +6,9 @@
 /*   By: amalangu <amalangu@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 08:51:47 by amalangu          #+#    #+#             */
-/*   Updated: 2025/11/06 11:40:35 by amalangu         ###   ########.fr       */
+/*   Updated: 2025/11/06 12:30:57 by amalangu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.class.hpp"
-
-using namespace std;
-
 int main(void) {
-	ClapTrap first("GOAT");
-	first.beRepaired(10);
-	first.attack("RATIO");
-	first.attack("RATIO");
-	first.attack("RATIO");
-	first.attack("RATIO");
-	first.attack("RATIO");
-	first.attack("RATIO");
-	first.attack("RATIO");
-	first.attack("RATIO");
-	first.attack("RATIO");
-	first.attack("RATIO");
-	first.attack("RATIO");
-	first.attack("RATIO");
-	first.beRepaired(10);
-	first.takeDamage(5);
-	first.takeDamage(15);
-	first.takeDamage(15);
-	first.takeDamage(15);
-	return 0;
 }
